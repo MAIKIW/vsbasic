756e293ecb1c159b4ad4ae40adcb6302aa62ef6c
ハジメ <117750380+MAIKIW@users.noreply.github.com>
2023-11-13 15:10:42 +0700

    Create exit.vb

---
+++ exit.vb
@@ -0,0 +1,5 @@
+ If MessageBox.Show("คุณต้องการออกจากโปรแกรมหรือไม่", "ระวัง", MessageBoxButtons.OKCancel,
+                           MessageBoxIcon.Information) = Windows.Forms.DialogResult.OK Then
+            End
+        End If
+    End Sub
